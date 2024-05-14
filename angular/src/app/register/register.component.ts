512b9d2120cf7c27996572e1816b6fd34b6bbdc3
SapphireLinXu <linsapphire@gmail.com>
2024-05-14 17:34:12 +0800

    reset func/email check

--- angular/src/app/register/register.component.ts
+++ angular/src/app/register/register.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
+import { MatInput, MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
@@ -13,12 +15,26 @@ import { MatIcon, MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  @ViewChild("userNameInput")
+  userNameInput!: ElementRef<MatInput>;
+  @ViewChild("emailInput")
+  emailInput!: ElementRef<MatInput>;
+  @ViewChild("passwordInput")
+  passwordInput!: ElementRef<MatInput>;
+  @ViewChild("confirmPasswordInput")
+  confirmPasswordInput!: ElementRef<MatInput>;
+
+
   hidePassword = true;
   hideConfirmPassword = true;
   constructor(private router: Router) { }
@@ -27,6 +43,24 @@ export class RegisterComponent {
   }
   navigate(path: string) {
     this.router.navigate([path]); // navigate to the path
+  }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  reset(){
+    this.userNameInput.nativeElement.value = '';
+    this.emailInput.nativeElement.value = '';
+    this.passwordInput.nativeElement.value = '';
+    this.confirmPasswordInput.nativeElement.value = '';
+
   }
 
 }
