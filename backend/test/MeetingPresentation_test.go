5a8657140903fef60c99cbf04e48253ada60eb6b
312605015 <fish312605015@gmail.com>
2024-05-18 20:42:56 +0800

    feat:meeting presentationTest addGetAllMeetingAndGetMeetingByRoomIdAndDate

--- backend/test/MeetingPresentation_test.go
+++ backend/test/MeetingPresentation_test.go
@@ -7,6 +7,7 @@ import (
 	"meeting-center/src/presentations"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
@@ -37,6 +38,15 @@ func (m *mockMeetingService) GetMeeting(id string) (*models.Meeting, error) {
 	return args.Get(0).(*models.Meeting), args.Error(1)
 }
 
+func (m *mockMeetingService) GetAllMeetings() ([]*models.Meeting, error) {
+	args := m.Called()
+	return args.Get(0).([]*models.Meeting), args.Error(1)
+}
+
+func (m *mockMeetingService) GetMeetingsByRoomIdAndDate(roomID int, date time.Time) ([]*models.Meeting, error) {
+	args := m.Called(roomID, date)
+	return args.Get(0).([]*models.Meeting), args.Error(1)
+}
 func TestCreateMeeting(t *testing.T) {
 	meeting := models.Meeting{
 		ID:          "1",
@@ -120,6 +130,44 @@ func TestGetMeeting(t *testing.T) {
 	r.GET("/meeting/:id", mp.GetMeeting)
 
 	req := httptest.NewRequest("GET", "/meeting/1", nil)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
+
+func TestGetAllMeetings(t *testing.T) {
+	meetings := []*models.Meeting{{ID: "1", Title: "Board Meeting", Description: "Annual Board Meeting"}}
+	mockMeetingService := new(mockMeetingService)
+	mockMeetingService.On("GetAllMeetings").Return(meetings, nil)
+	mp := presentations.NewMeetingPresentation(mockMeetingService)
+
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+	r.GET("/meetings", mp.GetAllMeetings)
+
+	req := httptest.NewRequest("GET", "/meetings", nil)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
+
+func TestGetMeetingsByRoomIdAndDate(t *testing.T) {
+	meetings := []*models.Meeting{{ID: "2", Title: "Team Meeting", Description: "Quarterly Planning"}}
+	roomID := 101
+	date := time.Now()
+	mockMeetingService := new(mockMeetingService)
+	mockMeetingService.On("GetMeetingsByRoomIdAndDate", roomID, date).Return(meetings, nil)
+	mp := presentations.NewMeetingPresentation(mockMeetingService)
+
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+	r.GET("/meetingsByRoomAndDate", mp.GetMeetingsByRoomIdAndDate)
+
+	req := httptest.NewRequest("GET", "/meetingsByRoomAndDate?room_id=101&date="+date.Format("2006-01-02"), nil)
 
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
