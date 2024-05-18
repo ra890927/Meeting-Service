56be733143ff2e3304319254d99f93fa170ff1ea
312605015 <fish312605015@gmail.com>
2024-05-18 18:39:28 +0800

    feat:meeting domainTest addGetAllRooms

--- backend/test/RoomDomain_test.go
+++ backend/test/RoomDomain_test.go
@@ -33,15 +33,21 @@ func (m *MockRoomRepo) GetRoom(id string) (*models.Room, error) {
 	return args.Get(0).(*models.Room), args.Error(1)
 }
 
+func (m *MockRoomRepo) GetAllRooms() ([]*models.Room, error) {
+	args := m.Called()
+	return args.Get(0).([]*models.Room), args.Error(1)
+}
+
 func TestDomainCreateRoom(t *testing.T) {
+	mockRoomRepo := new(MockRoomRepo)
+	rd := domains.NewRoomDomain(mockRoomRepo)
 	room := &models.Room{
 		RoomName: "Conference Room A",
 		Type:     "Board Meeting",
 		Capacity: 10,
 	}
-	mockRoomRepo := new(MockRoomRepo)
+
 	mockRoomRepo.On("CreateRoom", room).Return(room, nil)
-	rd := domains.NewRoomDomain(mockRoomRepo)
 	createdRoom, err := rd.CreateRoom(room)
 
 	assert.NoError(t, err)
@@ -49,42 +55,69 @@ func TestDomainCreateRoom(t *testing.T) {
 }
 
 func TestDomainUpdateRoom(t *testing.T) {
+	mockRoomRepo := new(MockRoomRepo)
+	rd := domains.NewRoomDomain(mockRoomRepo)
 	id := "1"
 	room := &models.Room{
 		RoomName: "Updated Conference Room",
 		Type:     "Executive Meeting",
 		Capacity: 12,
 	}
-	mockRoomRepo := new(MockRoomRepo)
+
 	mockRoomRepo.On("UpdateRoom", id, room).Return(nil)
-	rd := domains.NewRoomDomain(mockRoomRepo)
 	err := rd.UpdateRoom(id, room)
 
 	assert.NoError(t, err)
 }
 
 func TestDomainDeleteRoom(t *testing.T) {
-	id := "1"
 	mockRoomRepo := new(MockRoomRepo)
-	mockRoomRepo.On("DeleteRoom", id).Return(nil)
 	rd := domains.NewRoomDomain(mockRoomRepo)
+	id := "1"
+
+	mockRoomRepo.On("DeleteRoom", id).Return(nil)
 	err := rd.DeleteRoom(id)
 
 	assert.NoError(t, err)
 }
 
 func TestDomainGetRoom(t *testing.T) {
+	mockRoomRepo := new(MockRoomRepo)
+	rd := domains.NewRoomDomain(mockRoomRepo)
 	id := "1"
 	room := &models.Room{
 		RoomName: "Conference Room A",
 		Type:     "Board Meeting",
 		Capacity: 10,
 	}
-	mockRoomRepo := new(MockRoomRepo)
+
 	mockRoomRepo.On("GetRoom", id).Return(room, nil)
-	rd := domains.NewRoomDomain(mockRoomRepo)
 	fetchedRoom, err := rd.GetRoom(id)
 
 	assert.NoError(t, err)
 	assert.Equal(t, room, fetchedRoom)
+}
+
+func TestDomainGetAllRooms(t *testing.T) {
+	mockRoomRepo := new(MockRoomRepo)
+	rd := domains.NewRoomDomain(mockRoomRepo)
+	rooms := []*models.Room{
+		{
+			RoomName: "Conference Room A",
+			Type:     "Board Meeting",
+			Capacity: 10,
+		},
+		{
+			RoomName: "Conference Room B",
+			Type:     "Seminar",
+			Capacity: 20,
+		},
+	}
+
+	mockRoomRepo.On("GetAllRooms").Return(rooms, nil)
+	allRooms, err := rd.GetAllRooms()
+
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(allRooms))
+	assert.Equal(t, rooms, allRooms)
 }
