9d73f8aedebba24e5db284a1c04d0721bceb03a4
312605015 <fish312605015@gmail.com>
2024-05-20 22:29:01 +0800

    fix:room model DelSwagger

--- backend/src/models/Room.go
+++ backend/src/models/Room.go
@@ -6,11 +6,11 @@ import (
 
 // Room defines the structure for a meeting room
 type Room struct {
-	ID        int       `json:"id" gorm:"primaryKey" swaggerignore:"true"`
+	ID        int       `json:"id" gorm:"primaryKey"`
 	RoomName  string    `json:"room_name"`
 	Type      string    `json:"type"`
 	Rules     []string  `json:"rules"`
 	Capacity  int       `json:"capacity"`
-	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime" swaggerignore:"true"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime" swaggerignore:"true"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
