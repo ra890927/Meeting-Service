fce4e75dd3cfe9abd6aea8929c4f249441ed8146
hsuanyu414 <hsuanyu414@gmail.com>
2024-05-21 01:04:35 +0800

    fix: AdminRequire middleware
    1. middleware to convert from uint64 to uint

--- backend/src/middlewares/AdminRequire.go
+++ backend/src/middlewares/AdminRequire.go
@@ -8,10 +8,11 @@ func AdminRequire() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Get the user ID from the context
 		userID, _ := c.Get("validate_user_id")
+		userIDInt := uint(userID.(uint64))
 
 		// check if the user is an admin
 		// if not, return 403
-		if userID != 1 {
+		if userIDInt != 1 {
 			c.AbortWithStatusJSON(403, gin.H{"error": "Forbidden"})
 			return
 		}
