d57e4b257da37f51b00cb7cd31e8f124a3ce8c03
312605015 <fish312605015@gmail.com>
2024-05-20 23:01:40 +0800

    fix: meeting domain

--- backend/src/domains/MeetingDomain.go
+++ backend/src/domains/MeetingDomain.go
@@ -19,10 +19,10 @@ type meetingDomain struct {
 	MeetingRepo repos.MeetingRepo
 }
 
-func NewMeetingDomain(opt ...repos.MeetingRepo) MeetingDomain {
-	if len(opt) == 1 {
+func NewMeetingDomain(MeetingRepoArg ...repos.MeetingRepo) MeetingDomain {
+	if len(MeetingRepoArg) == 1 {
 		return &meetingDomain{
-			MeetingRepo: opt[0],
+			MeetingRepo: MeetingRepoArg[0],
 		}
 	} else {
 		return &meetingDomain{
@@ -31,7 +31,7 @@ func NewMeetingDomain(opt ...repos.MeetingRepo) MeetingDomain {
 	}
 }
 
-func (md *meetingDomain) CreateMeeting(meeting *models.Meeting) (*models.Meeting, error) {
+func (md meetingDomain) CreateMeeting(meeting *models.Meeting) (*models.Meeting, error) {
 	createdMeeting, err := md.MeetingRepo.CreateMeeting(meeting)
 	if err != nil {
 		return nil, err
@@ -39,7 +39,7 @@ func (md *meetingDomain) CreateMeeting(meeting *models.Meeting) (*models.Meeting
 	return createdMeeting, nil
 }
 
-func (md *meetingDomain) UpdateMeeting(id string, meeting *models.Meeting) error {
+func (md meetingDomain) UpdateMeeting(id string, meeting *models.Meeting) error {
 	err := md.MeetingRepo.UpdateMeeting(id, meeting)
 	if err != nil {
 		return err
@@ -47,7 +47,7 @@ func (md *meetingDomain) UpdateMeeting(id string, meeting *models.Meeting) error
 	return nil
 }
 
-func (md *meetingDomain) DeleteMeeting(id string) error {
+func (md meetingDomain) DeleteMeeting(id string) error {
 	err := md.MeetingRepo.DeleteMeeting(id)
 	if err != nil {
 		return err
@@ -55,7 +55,7 @@ func (md *meetingDomain) DeleteMeeting(id string) error {
 	return nil
 }
 
-func (md *meetingDomain) GetMeeting(id string) (*models.Meeting, error) {
+func (md meetingDomain) GetMeeting(id string) (*models.Meeting, error) {
 	meeting, err := md.MeetingRepo.GetMeeting(id)
 	if err != nil {
 		return nil, err
@@ -63,10 +63,10 @@ func (md *meetingDomain) GetMeeting(id string) (*models.Meeting, error) {
 	return meeting, nil
 }
 
-func (md *meetingDomain) GetAllMeetings() ([]*models.Meeting, error) {
+func (md meetingDomain) GetAllMeetings() ([]*models.Meeting, error) {
 	return md.MeetingRepo.GetAllMeetings()
 }
 
-func (md *meetingDomain) GetMeetingsByRoomIdAndDate(roomID int, date time.Time) ([]*models.Meeting, error) {
+func (md meetingDomain) GetMeetingsByRoomIdAndDate(roomID int, date time.Time) ([]*models.Meeting, error) {
 	return md.MeetingRepo.GetMeetingsByRoomIdAndDate(roomID, date)
 }
