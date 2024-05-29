3b4a8d5c923c54b7c0a8662f11e47740f476b983
SapphireLinXu <linsapphire@gmail.com>
2024-05-29 14:59:06 +0800

    modify: homepage button and add logo component

--- angular/src/app/homepage/homepage.component.ts
+++ angular/src/app/homepage/homepage.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { FooterComponent } from '../layout/footer/footer.component';
+import { LogoComponent } from '../layout/logo/logo.component';
 
 
 @Component({
@@ -9,7 +10,8 @@ import { FooterComponent } from '../layout/footer/footer.component';
   standalone: true,
   imports: [
     MatButtonModule,
-    FooterComponent
+    FooterComponent,
+    LogoComponent
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
