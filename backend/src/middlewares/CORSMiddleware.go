e2931c0b81a0a64099e22829637c15e64e06c504
hsuanyu414 <hsuanyu414@gmail.com>
2024-05-31 18:37:21 +0800

    fix: remove useless print

--- backend/src/middlewares/CORSMiddleware.go
+++ backend/src/middlewares/CORSMiddleware.go
@@ -1,8 +1,6 @@
 package middlewares
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,8 +10,6 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, token, samesite, Cookie")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
-		fmt.Println(c.Request.Header.Get("Authorization"))
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
