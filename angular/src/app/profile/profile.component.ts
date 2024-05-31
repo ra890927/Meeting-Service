56880966f64b4b38ecbc677516dc09a5c8169315
sapphirelinxu <linsapphire@gmail.com>
2024-05-31 15:44:26 +0800

    style: room management look style

--- angular/src/app/profile/profile.component.ts
+++ angular/src/app/profile/profile.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { UserSchedulerComponent } from '../user-scheduler/user-scheduler.component';
 import { HeaderComponent } from '../layout/header/header.component';
+import { FooterComponent } from '../layout/footer/footer.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [
     UserSchedulerComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
--- angular/src/app/reservation/reservation.component.ts
+++ angular/src/app/reservation/reservation.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../layout/header/header.component';
 import { RoomSchedulerComponent } from '../room-scheduler/room-scheduler.component';
+import { F } from '@angular/cdk/keycodes';
+import { FooterComponent } from '../layout/footer/footer.component';
 @Component({
   selector: 'app-reservation',
   standalone: true,
   imports: [
     HeaderComponent,
-    RoomSchedulerComponent
+    RoomSchedulerComponent,
+    FooterComponent
   ],
   templateUrl: './reservation.component.html',
   styleUrl: './reservation.component.css'
