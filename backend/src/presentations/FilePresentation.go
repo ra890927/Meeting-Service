50e66de0ad62ac379b131f06fb33e57e8a00318a
ra890927 <happy.890927410@gmail.com>
2024-06-02 16:57:25 +0800

    feat: add file api

---
+++ backend/src/presentations/FilePresentation.go
@@ -0,0 +1,258 @@
+package presentations
+
+import (
+	"fmt"
+	"meeting-center/src/models"
+	"meeting-center/src/services"
+	"mime/multipart"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
+
+type FilePresentation interface {
+	UploadFile(c *gin.Context)
+	GetFileURLsByMeetingID(c *gin.Context)
+	GetFile(c *gin.Context)
+	DeleteFile(c *gin.Context)
+}
+
+type filePresentation struct {
+	fileService services.FileService
+}
+
+type UploadFileForm struct {
+	MeetingID string                `form:"meeting_id"`
+	FormFile  *multipart.FileHeader `form:"file"`
+}
+
+type FileResponse struct {
+	Url        string `json:"url"`
+	UploaderID uint   `json:"uploader_id"`
+	FileName   string `json:"file_name"`
+}
+
+type UploadFileResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Data    struct {
+		Url        string `json:"url"`
+		UploaderID uint   `json:"uploader_id"`
+		FileName   string `json:"file_name"`
+	} `json:"data"`
+}
+
+type GetFileURLsByMeetingIDResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Data    []struct {
+		Url        string `json:"url"`
+		UploaderID uint   `json:"uploader_id"`
+		FileName   string `json:"file_name"`
+	} `json:"data"`
+}
+
+type DeleteFileResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+func NewFilePresentation(fileServiceArgs ...services.FileService) FilePresentation {
+	if len(fileServiceArgs) == 0 {
+		return filePresentation{fileService: services.NewFileService()}
+	} else if len(fileServiceArgs) == 1 {
+		return filePresentation{fileService: fileServiceArgs[0]}
+	} else {
+		panic("too many arguments")
+	}
+}
+
+// @Summary Upload a file
+// @Description Upload a file
+// @Tags File
+// @Accept json
+// @Produce json
+// @Param file form UploadFileForm true "File details"
+// @Success 200 {object} UploadFileResponse
+// @Router /file [post]
+func (fp filePresentation) UploadFile(c *gin.Context) {
+	var form UploadFileForm
+	var response UploadFileResponse
+
+	if err := c.ShouldBind(&form); err != nil {
+		response = UploadFileResponse{
+			Status:  "error",
+			Message: err.Error(),
+		}
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	if form.FormFile == nil {
+		response = UploadFileResponse{
+			Status:  "error",
+			Message: "No file uploaded",
+		}
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	src, err := form.FormFile.Open()
+	if err != nil {
+		response = UploadFileResponse{
+			Status:  "error",
+			Message: err.Error(),
+		}
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	defer src.Close()
+
+	ctx := c.Request.Context()
+	operator := c.MustGet("validate_user").(models.User)
+	file := &models.File{
+		MeetingID:  form.MeetingID,
+		UploaderID: operator.ID,
+		FileName:   form.FormFile.Filename,
+		FileExt:    filepath.Ext(form.FormFile.Filename),
+	}
+
+	url, err := fp.fileService.UploadFile(ctx, operator, file, src)
+	if err != nil {
+		response = UploadFileResponse{
+			Status:  "error",
+			Message: err.Error(),
+		}
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response = UploadFileResponse{
+		Status:  "success",
+		Message: "File uploaded",
+		Data: FileResponse{
+			Url:        url,
+			UploaderID: operator.ID,
+			FileName:   form.FormFile.Filename,
+		},
+	}
+
+	c.JSON(http.StatusOK, response)
+}
+
+// @Summary Get files by meeting ID
+// @Description Get files by meeting ID
+// @Tags File
+// @Param meeting_id path string true "Meeting ID"
+// @Success 200 {object} GetFileURLsByMeetingIDResponse
+// @Router /file/getFileURLsByMeetingID [get]
+func (fp filePresentation) GetFileURLsByMeetingID(c *gin.Context) {
+	var response GetFileURLsByMeetingIDResponse
+
+	meetingID := c.Param("meeting_id")
+	if len(meetingID) == 0 {
+		response = GetFileURLsByMeetingIDResponse{
+			Status:  "error",
+			Message: "Invalid request",
+		}
+
+		c.JSON(http.StatusBadRequest, response)
+	}
+
+	ctx := c.Request.Context()
+	operator := c.MustGet("validate_user").(models.User)
+	files, urls, err := fp.fileService.GetAllSignedURLsByMeeting(ctx, operator, meetingID)
+	if err != nil {
+		response = GetFileURLsByMeetingIDResponse{
+			Status:  "error",
+			Message: err.Error(),
+		}
+
+		c.JSON(http.StatusBadRequest, response)
+	}
+
+	response.Status = "success"
+	response.Message = "Files retrieved"
+	for i := 0; i < len(files); i++ {
+		response.Data = append(response.Data, FileResponse{
+			Url:        urls[i],
+			UploaderID: files[i].UploaderID,
+			FileName:   files[i].FileName,
+		})
+	}
+
+	c.JSON(http.StatusOK, response)
+}
+
+// @Summary Get a file
+// @Description Get a file
+// @Tags File
+// @Param id path string true "File ID"
+// @Success 200 {object} DeleteFileResponse
+// @Router /file/{id} [delete]
+func (fp filePresentation) GetFile(c *gin.Context) {
+	id := c.Param("id")
+	if len(id) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid request",
+		})
+		return
+	}
+
+	ctx := c.Request.Context()
+	operator := c.MustGet("validate_user").(models.User)
+	signedUrl, err := fp.fileService.GetSignedURL(ctx, operator, id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	fmt.Println("redirect")
+
+	c.Redirect(http.StatusMovedPermanently, signedUrl)
+}
+
+// @Summary Delete a file
+// @Description Delete a file
+// @Tags File
+// @Param id path string true "File ID"
+// @Success 200 {object} DeleteFileResponse
+// @Router /file/{id} [delete]
+func (fp filePresentation) DeleteFile(c *gin.Context) {
+	var response DeleteFileResponse
+
+	id := c.Param("id")
+	if len(id) == 0 {
+		response = DeleteFileResponse{
+			Status:  "error",
+			Message: "Invalid request",
+		}
+
+		c.JSON(http.StatusBadRequest, response)
+	}
+
+	ctx := c.Request.Context()
+	operator := c.MustGet("validate_user").(models.User)
+	if err := fp.fileService.DeleteFile(ctx, operator, id); err != nil {
+		response = DeleteFileResponse{
+			Status:  "error",
+			Message: err.Error(),
+		}
+
+		c.JSON(http.StatusBadRequest, response)
+	}
+
+	response = DeleteFileResponse{
+		Status:  "success",
+		Message: "File deleted",
+	}
+
+	c.JSON(http.StatusOK, response)
+}
---
+++ backend/src/services/FileService.go
@@ -0,0 +1,178 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"meeting-center/src/domains"
+	"meeting-center/src/models"
+	"meeting-center/src/utils"
+	"mime/multipart"
+	"path/filepath"
+	"slices"
+)
+
+type fileService struct {
+	gcsDomain     domains.GcsDomain
+	fileDomain    domains.FileDomain
+	meetingDomain domains.MeetingDomain
+}
+
+type FileService interface {
+	UploadFile(ctx context.Context, operator models.User, file *models.File, object multipart.File) (string, error)
+	GetAllSignedURLsByMeeting(ctx context.Context, operator models.User, meetingID string) ([]models.File, []string, error)
+	GetSignedURL(ctx context.Context, operater models.User, id string) (string, error)
+	DeleteFile(ctx context.Context, operater models.User, id string) error
+}
+
+func NewFileService(fileServiceArgs ...interface{}) FileService {
+	gcsDomain := domains.NewGcsDomain()
+	fileDomain := domains.NewFileDomain()
+	meetingDomain := domains.NewMeetingDomain()
+
+	for _, arg := range fileServiceArgs {
+		switch val := arg.(type) {
+		case domains.GcsDomain:
+			gcsDomain = val
+		case domains.FileDomain:
+			fileDomain = val
+		case domains.MeetingDomain:
+			meetingDomain = val
+		default:
+			panic("Unknown type")
+		}
+	}
+
+	return fileService{
+		gcsDomain:     gcsDomain,
+		fileDomain:    fileDomain,
+		meetingDomain: meetingDomain,
+	}
+}
+
+func (fs fileService) UploadFile(ctx context.Context, operator models.User, file *models.File, object multipart.File) (string, error) {
+	permission, err := fs.getPermissionByFile(operator, *file)
+	if err != nil {
+		return "", err
+	}
+
+	if !utils.CheckPermission(permission, utils.Upload) {
+		return "", errors.New("only admin and participant can upload file")
+	}
+
+	file.UploaderID = operator.ID
+	// if success, it will get file id
+	if err := fs.fileDomain.UploadFile(file); err != nil {
+		return "", err
+	}
+
+	ext := filepath.Ext(file.FileName)
+	if err := fs.gcsDomain.UploadFile(ctx, object, file.ID, ext); err != nil {
+		return "", err
+	}
+
+	return fs.gcsDomain.GetSignedURL(ctx, file.ID, file.FileExt)
+}
+
+func (fs fileService) GetAllSignedURLsByMeeting(ctx context.Context, operator models.User, meetingID string) ([]models.File, []string, error) {
+	permission, err := fs.getPermissionByMeetingID(operator, meetingID)
+	if err != nil {
+		return []models.File{}, []string{}, err
+	}
+	if !utils.CheckPermission(permission, utils.Read) {
+		return []models.File{}, []string{}, errors.New("only admin and participant can download file")
+	}
+
+	files, err := fs.fileDomain.GetAllFilesByMeetingID(meetingID)
+	if err != nil {
+		return []models.File{}, []string{}, err
+	}
+
+	urls := make([]string, 0)
+	for _, file := range files {
+		url, err := fs.gcsDomain.GetSignedURL(ctx, file.ID, file.FileExt)
+		if err != nil {
+			return []models.File{}, []string{}, err
+		}
+		urls = append(urls, url)
+	}
+
+	return files, urls, nil
+}
+
+func (fs fileService) GetSignedURL(ctx context.Context, operater models.User, id string) (string, error) {
+	file, err := fs.fileDomain.GetFile(id)
+	if err != nil {
+		return "", err
+	}
+
+	permission, err := fs.getPermissionByFile(operater, file)
+	if err != nil {
+		return "", err
+	}
+
+	if !utils.CheckPermission(permission, utils.Read) {
+		return "", errors.New("only admin and participant can download file")
+	}
+
+	return fs.gcsDomain.GetSignedURL(ctx, file.ID, file.FileExt)
+}
+
+func (fs fileService) DeleteFile(ctx context.Context, operater models.User, id string) error {
+	file, err := fs.fileDomain.GetFile(id)
+	if err != nil {
+		return err
+	}
+
+	permission, err := fs.getPermissionByFile(operater, file)
+	if err != nil {
+		return err
+	}
+
+	if !utils.CheckPermission(permission, utils.Delete) {
+		return errors.New("only admin, organizer, and uploader can delete file")
+	}
+
+	if err := fs.gcsDomain.DeleteFile(ctx, file.ID, file.FileExt); err != nil {
+		return err
+	}
+
+	if err := fs.fileDomain.DeleteFile(file.ID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (fs fileService) getPermissionByFile(operator models.User, file models.File) (utils.Permission, error) {
+	permission := utils.Empty
+
+	meeting, err := fs.meetingDomain.GetMeeting(file.MeetingID)
+	if err != nil {
+		return utils.Empty, nil
+	}
+
+	if operator.Role == "admin" || operator.ID == file.UploaderID || operator.ID == meeting.OrganizerID {
+		permission = utils.Upload | utils.Delete | utils.Read
+	} else if slices.Contains(meeting.Participants, operator.ID) {
+		permission = utils.Upload | utils.Read
+	}
+
+	return permission, nil
+}
+
+func (fs fileService) getPermissionByMeetingID(operator models.User, meetingID string) (utils.Permission, error) {
+	permission := utils.Empty
+
+	meeting, err := fs.meetingDomain.GetMeeting(meetingID)
+	if err != nil {
+		return utils.Empty, nil
+	}
+
+	if operator.Role == "admin" || operator.ID == meeting.OrganizerID {
+		permission = utils.Upload | utils.Delete | utils.Read
+	} else if slices.Contains(meeting.Participants, operator.ID) {
+		permission = utils.Upload | utils.Read
+	}
+
+	return permission, nil
+}
