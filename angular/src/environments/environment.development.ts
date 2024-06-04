289d5fbb446b36bce61e5c73f2c360f96856a096
hsuanyu414 <hsuanyu414@gmail.com>
2024-06-05 03:35:38 +0800

    fix:remove angular

--- angular/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'http://140.113.215.132:8080/api/v1/'//change to backend server
-};
--- angular/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://140.113.215.132:8080/api/v1/'
-  };
